2eafbaf6dbcbb5cf895539a5e82b42b81ca00425
Mark Wei <markwei@gmail.com>
2025-10-19 00:39:10 -0700

    Switch from Android navigation lib to common nav component.

--- samples/android/src/main/java/io/github/pingpongboss/explodedlayers/samples/android/MainActivity.android.kt
+++ samples/android/src/main/java/io/github/pingpongboss/explodedlayers/samples/android/MainActivity.android.kt
@@ -4,98 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.TextAutoSize
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import io.github.pingpongboss.explodedlayers.samples.android.theme.ExplodedLayersSampleTheme
-import io.github.pingpongboss.explodedlayers.samples.common.AppScreen
-import io.github.pingpongboss.explodedlayers.samples.common.DialerApp
-import io.github.pingpongboss.explodedlayers.samples.common.MessagesApp
 import io.github.pingpongboss.explodedlayers.samples.common.navigation.TabNavItem
+import io.github.pingpongboss.explodedlayers.samples.common.navigation.TabNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        setContent { ExplodedLayersSampleTheme { MainScreen() } }
-    }
-}
-
-@Composable
-private fun MainScreen() {
-    val navController = rememberNavController()
-    val navItems = listOf(TabNavItem.Buttons, TabNavItem.Dialer, TabNavItem.Messages)
-
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
-
-                navItems.forEach { screen ->
-                    NavigationBarItem(
-                        selected =
-                            currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                        onClick = {
-                            navController.navigate(screen.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        icon = { Icon(screen.icon, contentDescription = screen.label) },
-                        label = { Text(screen.label) },
-                    )
-                }
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(top = innerPadding.calculateTopPadding()),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                text = "https://github.com/pingpongboss/compose-exploded-layers",
-                autoSize = TextAutoSize.StepBased(),
-                maxLines = 1,
-            )
-
-            NavHost(
-                navController = navController,
-                startDestination = TabNavItem.Buttons.route,
-                modifier = Modifier.padding(bottom = innerPadding.calculateBottomPadding()),
-            ) {
-                composable(TabNavItem.Buttons.route) {
-                    ExplodedLayersSampleTheme(darkTheme = false) { ButtonsScreen() }
-                }
-                composable(TabNavItem.Dialer.route) {
-                    ExplodedLayersSampleTheme(darkTheme = true) { AppScreen { DialerApp() } }
-                }
-                composable(TabNavItem.Messages.route) {
-                    ExplodedLayersSampleTheme(darkTheme = true) { AppScreen { MessagesApp() } }
-                }
+        setContent {
+            ExplodedLayersSampleTheme {
+                TabNavigation(
+                    tabs =
+                        listOf(
+                            TabNavItem.Buttons {
+                                ExplodedLayersSampleTheme(darkTheme = false) { ButtonsScreen() }
+                            },
+                            TabNavItem.Simple,
+                            TabNavItem.Dialer,
+                            TabNavItem.Messages,
+                        ),
+                    startDestinationLabel = TabNavItem.Buttons.label,
+                    header = {
+                        Text(
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                            text = "https://github.com/pingpongboss/compose-exploded-layers",
+                            autoSize = TextAutoSize.StepBased(),
+                            maxLines = 1,
+                        )
+                    },
+                )
             }
         }
     }
